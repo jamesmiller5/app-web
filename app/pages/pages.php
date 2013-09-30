d9b59c7bf722149340479c9d98b73e8808bb654c
Mitchell Mounts <mdonmounts@gmail.com>
2013-09-30 18:02:57 -0400

    Fixed graph demo, moved JS files to JS folder

--- app/pages/pages.php
+++ app/pages/pages.php
@@ -111,8 +111,8 @@ class Graph extends Page {
 	function render() {
 		echo <<<HTML
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script src="springy.js"></script>
-<script src="springyui.js"></script>
+<script src="/js/springy.js"></script>
+<script src="/js/springyui.js"></script>
 <script>
 var graph = new Springy.Graph();
 
@@ -151,7 +151,8 @@ jQuery(function(){
 });
 </script>
 		
-		<canvas id="springydemo" width="640" height="480" />
+		<canvas id="springydemo" width="640" height="480" >
+		</canvas>
 HTML;
 	}
 }
