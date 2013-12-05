7a1b92d00da616ba50f372e2d2ad81d1dfeb3b09
James Miller V <jim@jmiller5.com>
2013-12-04 19:44:18 -0500

    Added ability to register a callback instead of an entire page object

--- app/Router.class.php
+++ app/Router.class.php
@@ -12,9 +12,31 @@ class Router {
 		return Router::$default;
 	}
 
+	function registerHandler($path, callable $callme ) {
+		if( !isset( $path ) )
+			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " \$path was null" );
+
+		if( $callme == null )
+			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " \$page was null" );
+
+		if( is_array( $path ) ) {
+			foreach( $path as $value ) {
+				$this->registerRaw( $value, $callme );
+			}
+
+			return;
+		}
+
+		$path = trim( $path );
+		$this->handlers[$path] = $callme;
+	}
+
 	function register($path, Page $page ) {
 		if( !isset( $path ) )
 			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " \$path was null" );
+
+		if( $page== null )
+			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " \$page was null" );
 
 		if( is_array( $path ) ) {
 			foreach( $path as $value ) {
@@ -25,11 +47,6 @@ class Router {
 		}
 
 		$path = trim( $path );
-
-		if( $page== null )
-			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " \$page was null" );
-
-
 		$this->handlers[$path] = $page;
 	}
 
@@ -42,57 +59,61 @@ class Router {
 			$inPath = "/";
 		}
 
+		$bestHandler = null;
+
 		//easy case, path matches exactly
 		if( isset( $this->handlers[$inPath] ) ) {
-			$this->handlers[$inPath]->handle($request);
-			return;
-		}
+			$bestHandler = $this->handlers[$inPath];
+		} else {
+			$max = 0;
 
-		$bestPage = null;
-		$max = 0;
+			//non easy case, find longest match counting '/'s and use that
+			$r = strlen($inPath);
+			foreach( $this->handlers as $matchPath => $handler ) {
+				$c = 0;
+				$i = 0;
+				$l = strlen($matchPath);
 
-		//non easy case, find longest match counting '/'s and use that
-		$r = strlen($inPath);
-		foreach( $this->handlers as $matchPath => $page ) {
-			$c = 0;
-			$i = 0;
-			$l = strlen($matchPath);
+				while( true ) {
+					//matched a whole path, it wins (so far)
+					if( $i == $l ) {
+						$c++;
+						break;
+					}
 
-			while( true ) {
-				//matched a whole path, it wins (so far)
-				if( $i == $l ) {
-					$c++;
-					break;
+					//end of the input, just stop
+					if( $i == $r ) {
+						break;
+					}
+
+					//check if we made it to the next slash
+					if( $matchPath[$i] == '/' ) {
+						$c++;
+					}
+
+					//check if we should continue matching
+					if( $matchPath[$i] != $inPath[$i] ) {
+						break;
+					}
+
+					$i++;
 				}
 
-				//end of the input, just stop
-				if( $i == $r ) {
-					break;
+				if( $c > $max ) {
+					$bestHandler = $handler;
+					$max = $c;
 				}
-
-				//check if we made it to the next slash
-				if( $matchPath[$i] == '/' ) {
-					$c++;
-				}
-
-				//check if we should continue matching
-				if( $matchPath[$i] != $inPath[$i] ) {
-					break;
-				}
-
-				$i++;
 			}
 
-			if( $c > $max ) {
-				$bestPage = $page;
-				$max = $c;
+			if( $bestHandler == null ) {
+				throw new Exception( __FILE__ . ":" . __FUNCTION__ . " couldn't route this request");
 			}
 		}
 
-		if( $bestPage == null ) {
-			throw new Exception( __FILE__ . ":" . __FUNCTION__ . " couldn't route this request");
+		if( is_callable( $bestHandler ) ) {
+			$bestHandler($request);
+		} else {
+			$bestHandler->handle($request);
 		}
-
-		$bestPage->handle( $request );
 	}
 }
