08e0287421aa93c07fe81d4f429059698f11f734
Andrew Mack <abmack@purdue.edu>
2013-12-05 01:07:08 -0500

    re-added foo@bar email for old-times sake

--- util/insert-database.sh
+++ util/insert-database.sh
@@ -3,6 +3,10 @@ APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 sqlite3 -batch "$APPDIR/database.sqlite" <<INSERTDATA
 PRAGMA foreign_keys = ON;
 
+insert into Email values("foo@bar", "thisisfoobar123");
+insert into Email values("bar@foo", "456barfoo");
+insert into User(email,password) values("foo@bar", "number");
+
 insert into Email values("harold@app.com", "htoken");
 insert into Email values("jennifer@app.com", "jetoken");
 insert into Email values("joseph@app.com", "jotoken");
